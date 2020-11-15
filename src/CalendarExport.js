559401a2a66ca896d9d9c94c1b81a9a6a3b9edc8
Albert Yan <albertzy@usc.edu>
2020-11-15 20:53:16 +0800

    create testing files

--- src/CalendarExport.js
+++ src/CalendarExport.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function CalendarExport(){
 
-    return(
-        <div>
-            Calendar Export
+    useEffect(() => {
+        document.title = "Export to Google Calendar | Microplanner"
+    }, [])
+
+    return (
+        <div className="justify-content-center">
+            <div className="row centerTitle">
+                New function coming soon!
+            </div>
         </div>
+
     )
 }
