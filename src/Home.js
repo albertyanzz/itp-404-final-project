64592293a56a73b7568940300368c949d4d34070
Albert Yan <albertzy@usc.edu>
2020-11-15 16:56:48 +0800

    removed guest option

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useContext } from "react";
 import './App.css';
-import { Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { DataStoreContext } from "./contexts";
@@ -95,13 +94,6 @@ export default function Home(){
 		<div className="homeContainer">
 			<div className="row homeFont homeItem">
 				Microplanner
-			</div>
-			<div className="row homeItem">
-				{ !isLoggedIn &&
-					<Link to="/tasks" className="btn btn-light">
-						Use as Guest
-					</Link>
-				}
 			</div>
 			<div id="googleButton">
 			</div>
